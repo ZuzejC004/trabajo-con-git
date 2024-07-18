67dae0087bc36f274c6f0098dcd4b4fe53c5ecb7
ZuzejC004 <zuzejcrespo004@gmail.com>
2024-07-17 20:21:32 -0400

    cambios en mi programa

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/* Zuzej Crespo */
+
+int main(int argc, char** argv) {
+	int num=0
+	cout<<"Ingresa un numero";
+	cin>>num
+	if(num>=0){
+	cout<<"el valor Absoluto es:" <<num;
+	}
+      else{
+      	cout<<"El valor Absoluto es:"<<num*-1;
+  	}
+	return 0;
+}
