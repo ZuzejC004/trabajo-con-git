dbb667b9961b821ae87a2dc0c8af46f8a6faf6f8
ZuzejC004 <zuzejcrespo004@gmail.com>
2024-07-17 20:52:04 -0400

    cambios de mi primer commit

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 /* Zuzej Crespo */
 
 int main(int argc, char** argv) {
-	int num=0
-	cout<<"Ingresa un numero";
-	cin>>num
+	int num=0;
+	cout << "Ingresa un numero";
+	cin << num=0;
 	if(num>=0){
-	cout<<"el valor Absoluto es:" <<num;
+	cout<<"el valor Absoluto es:"<<num;
 	}
       else{
       	cout<<"El valor Absoluto es:"<<num*-1;
